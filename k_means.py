190a0f7bcac301cf8a80a6523dd3666834f0022d
anirudhkuchibhatla <ask9@rice.edu>
2019-01-27 16:07:36 -0600

    Performed k means clustering on test data

--- k_means.py
+++ k_means.py
@@ -1,1 +1,9 @@
-print(dog)
+import pandas as pd
+from sklearn.cluster import KMeans
+
+test_data = pd.read_csv("test.csv")
+
+kmeans = KMeans(n_clusters=10).fit(test_data)
+
+predictions = [(i*2 +2) for i in kmeans.labels_]
+
