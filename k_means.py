e556607df054893021dc680cbb987519fcc0021e
anirudhkuchibhatla <ask9@rice.edu>
2019-01-27 19:59:36 -0600

    Modified to create labelled version that will be added to the test to label test

--- k_means.py
+++ k_means.py
@@ -1,9 +1,23 @@
 import pandas as pd
 from sklearn.cluster import KMeans
 
+train_data = pd.read_csv("train.csv")
 test_data = pd.read_csv("test.csv")
 
-kmeans = KMeans(n_clusters=10).fit(test_data)
+trained_rows = pd.DataFrame(columns=test_data.columns)
+labels = []
+while len(labels) < 10:
+    for index, row in train_data.iterrows():
+        if row['label'] not in labels:
+            labels.append(row['label'])
+            trained_rows = trained_rows.append(row)
+            print(row)
 
-predictions = [(i*2 +2) for i in kmeans.labels_]
+print(labels)
+print(trained_rows)
+#kmeans = KMeans(n_clusters=10).fit(test_data)
+
+#predictions = kmeans.labels
+
+
 
